0fc5dd60417fbd8de05aa52ea8dbc758727d8013
Sadia <sadiashah059@gmail.com>
2022-06-28 09:44:34 -0200

    some details added

--- middleware.js
+++ middleware.js
@@ -1,11 +1,5 @@
 const multer = require("multer")
 
-//body parses only parse text based data. Image would be in binary form, so we need to use multer
-//image is passed because it is the name of the input of type file in the ejs file names imageupload
-
-//destination: before using dest, multer give us an array of buffer. But when we give it dest field it would store the 
-//file at somepath of following format: image/ajflfi0df9d
-
 const fileStorage = multer.diskStorage({
     destination:(req, file, callback)=>{
         console.log("Hello World")
